f51fe28f9d885fb196c51f0cf5ba619d0df84bdc
clientweb <info@clientweb.ca>
2019-02-22 08:40:15 -0500

    RL: 2019-02-22
    Setup 856 xml writer

--- el_edi/EDI_RSS/WscieVendor/Xml856Writer.cs
+++ el_edi/EDI_RSS/WscieVendor/Xml856Writer.cs
@@ -31,7 +31,7 @@ namespace EDI_RSS.Helpers
 
             try
             {
-                CreateNew();
+                //CreateNew();
 
                 List<IDataRecord> RawData = GetData();
 
@@ -64,37 +64,41 @@ namespace EDI_RSS.Helpers
             }
         }
 
-        public void CreateNew()
-        {
-            DB_VIVA.HExecuteSQLNonQuery(@" 
-                INSERT INTO edi_856 (cobil_ident) 
-                SELECT cobil.ident 
-                FROM cobil
-                    WHERE   cobil.clientid = 30037 AND 
-                            cobil.ident > 14300 AND 
-                            NOT EXISTS(SELECT 1 FROM edi_856 AS edi_856e WHERE edi_856e.cobil_ident = cobil.ident); 
-                ALTER TABLE edi_856 AUTO_INCREMENT = 1;");
+        //public void CreateNew()
+        //{
+        //    DB_VIVA.HExecuteSQLNonQuery(@" 
+        //        INSERT INTO edi_856 (cobil_ident) 
+        //        SELECT cobil.ident 
+        //        FROM cobil
+        //            WHERE   cobil.clientid = 30037 AND 
+        //                    cobil.ident = 10 AND 
+        //                    NOT EXISTS(SELECT 1 FROM edi_856 AS edi_856e WHERE edi_856e.cobil_ident = cobil.ident); 
+        //        ALTER TABLE edi_856 AUTO_INCREMENT = 1;");
 
-            //update pour mettre le ship date
-            DateTime dte_now = DateTime.Now;
+        //    //update pour mettre le ship date
+        //    DateTime dte_now = DateTime.Now;
 
-            Params.Clear();
-            Params.Add("?dte_now", dte_now.ToString());
+        //    Params.Clear();
+        //    Params.Add("?dte_now", dte_now.ToString());
 
-            DB_VIVA.HExecuteSQLQuery(@"UPDATE edi_856 SET Ship_date = ?dte_now WHERE cobil_ident = 14333;", Params);
-        }
+        //    DB_VIVA.HExecuteSQLQuery(@"UPDATE edi_856 SET Ship_date = ?dte_now WHERE cobil_ident = 10;", Params);
+        //}
 
         public List<IDataRecord> GetData()
         {
             return DB_VIVA.HExecuteSQLQuery(@"
-                SELECT  
+                SELECT  				
                         cobil.ident AS cobil_ident, cobil.clientid AS cobil_clientid, cobil.clientpo AS cobil_clientpo, cobil.BIL_DTE AS cobil_bil_dte,
-                        edi_856.ident AS edi_856_ident, edi_856.filename AS edi_856_filename, edi_856.Ship_date AS edi_856_ship_date
+                        edi_856.ident AS edi_856_ident, edi_856.filename AS edi_856_filename, edi_856.Ship_date AS edi_856_ship_date,
+                        SUM(cobili.qty) AS cobili_qty
                 FROM cobil 
                 INNER JOIN edi_856 ON edi_856.cobil_ident = cobil.ident
+                INNER JOIN cobili ON cobil.ident = cobili.IDCOBIL
                 WHERE edi_856.Sent = false 
                       AND
-                      cobil.ident = 14333
+                      cobil.ident = 10 
+                      AND
+                      cobil.STATUT = 'C'
                 ");
         }
 
@@ -105,18 +109,19 @@ namespace EDI_RSS.Helpers
 
             return DB_VIVA.HExecuteSQLQuery(
                 @"SELECT  
-                        ivprod.code AS ivprod_code, ivprod.DESC AS ivprod_desc,
+                        COUNT(cocom.ident) AS nb_items,
+                        cobil.ident AS cobil_ident,      
                         edi_856.ident AS edi_856_ident, 
                         cobili.PO_NO AS cobili_po_no, cobili.QTY AS cobili_qty,
-                        cocom.CLIENTPO AS cocom_clientpo, cocom.ident AS cocom_ident  
+                        cocom.CLIENTPO AS cocom_clientpo, cocom.ident AS cocom_ident, cocom.CO_DTE AS cocom_co_dte
                 FROM cobil 
                 INNER JOIN cobili ON cobil.ident = cobili.idcobil
-                INNER JOIN ivprod ON ivprod.ident = cobili.idprod
                 INNER JOIN edi_856 ON edi_856.cobil_ident = cobil.ident
                 INNER JOIN cocom ON cocom.ident = cobili.IDCOM
                 WHERE   edi_856.Sent = false 
-                        AND
-                        cobil.ident = ?cobil_ident
+                        AND cocom.CLIENTID = 30037 
+                        AND cobil.ident = ?cobil_ident
+                GROUP BY cocom.ident
                 ORDER BY cobili.ident, cobili.line
                 ", Params);
         }
@@ -128,16 +133,29 @@ namespace EDI_RSS.Helpers
             Params.Add("?cocom_ident", cocom_ident);
 
             return DB_VIVA.HExecuteSQLQuery(
-                @"SELECT cobil.DEL_NAME,
-                         cobili.PO_NO AS cobili_po_no, cobili.QTY AS cobili_qty,
-                         cocom.ident AS cocom_ident
+                @"SELECT cobili.LINE AS cobili_line, cobili.QTY AS cobili_qty,
+                         cocom.ident AS cocom_ident,
+                         ivprod.ident AS ivprod_ident, ivprod.code AS ivprod_code, ivprod.DESC AS ivprod_desc,
+                         ivprixdcli.codecli AS ivprixdcli_codecli
                   FROM cobil 
                   INNER JOIN cobili ON cobil.ident = cobili.idcobil
                   INNER JOIN cocom ON cocom.ident = cobili.IDCOM
-                  WHERE cobil.ident = cobil_ident 
-                        AND 
-                        cocom.ident = ?cocom_ident
+                  INNER JOIN ivprod ON ivprod.ident = cobili.idprod
+                  LEFT JOIN ivprixdcli ON (ivprixdcli.idclient = cobil.CLIENTID AND ivprixdcli.idprod = cobili.IDPROD AND IFNULL(ivprixdcli.codecli, '') <> '')
+                  WHERE cobil.ident = ?cobil_ident 
+                       AND cocom.ident = ?cocom_ident
                   ORDER BY cobili.ident, cobili.line
+                ", Params);
+        }
+
+        public List<IDataRecord> GetShippedDate(string cobili_idcom, string cobili_idprod)
+        {
+            Params.Clear();
+            Params.Add("?cobili_idcom", cobili_idcom);
+            Params.Add("?cobili_idprod", cobili_idprod);
+
+            return DB_VIVA.HExecuteSQLQuery(
+                @"SELECT SUM(cobili.qty) AS sum_cobili_qty FROM vivadata4.cobili wHERE cobili.IDCOM = ?cobili_idcom and COBILI.IDPROD = ?cobili_idprod
                 ", Params);
         }
     }
@@ -157,7 +175,7 @@ namespace EDI_RSS.Helpers
 
             ClientID = Data["cobil_clientid"].ToString();
 
-            OutputFileName = $"{ClientID}-856OUT-{Data["cobil_ident"].ToString()}-{Base.ToAlphaNumeric(Data["cobil_clientpo"].ToString())}-{(DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalSeconds}";
+            OutputFileName = $"{ClientID.PadLeft(5, '0')}-856OUT-{Data["cobil_ident"].ToString()}-{Base.ToAlphaNumeric(Data["cobil_clientpo"].ToString())}-{(DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalSeconds}";
 
             XmlFilePath = Path.Combine(RSS_send_path, OutputFileName + ".xml");
 
@@ -209,24 +227,38 @@ namespace EDI_RSS.Helpers
                 WriteSegment("DTM", "Segment", "DTM01 : Date/Time Qualifier: Fixed : Estimated Delivery", "017",
                                                "DTM02 : Date : cobil_bil_dte", string.Format("{0:yyyyMMdd}", cobil_bil_dte.AddDays(1)));
 
+
                 WriteHLLoop1_Shipment(Data);
 
-                //details
+                WriteN1Loop1_arclient(EntityCode1.ST, EntityCode2.BuyerCode);
+                WriteN1Loop1_wscie(EntityCode1.SF, EntityCode2.SellerCode);
+
+                List<IDataRecord> items;
+                List<IDataRecord> sum_cobili_qty;
+                int Id = 2;  //Hierarchical ID Number
+                int Pid = 1; //Hierarchical Parent ID Number
+
                 foreach (var DataDetail in RawDataDetails)
                 {
-                    int QtyShipped = Convert.ToInt32(DataDetail["cobili_qty"]);
+                    WriteHLLoop1_Order(DataDetail, Id, Pid);
+                   
+                    items = mysql.GetItems(DataDetail["cobil_ident"].ToString(), DataDetail["cocom_ident"].ToString());
+                    Pid = Id;
+                    foreach (var item in items)
+                    {
+                        Id++;
+                        sum_cobili_qty = mysql.GetShippedDate(item["cocom_ident"].ToString(), item["ivprod_ident"].ToString());
+                        int ShippedToDate = Convert.ToInt32(sum_cobili_qty[0]["sum_cobili_qty"]) - Convert.ToInt32(item["cobili_qty"]);
+                        WriteHLLoop1_Item(item, ShippedToDate, Id, Pid);
 
-                    TotalNumberOfLineItem++;
-                    TotalNumberOfLineItemQty += QtyShipped;
+                        int QtyShipped = Convert.ToInt32(item["cobili_qty"]);
 
-                    WriteHLLoop1_Order(DataDetail);
-
+                        TotalNumberOfLineItem++;
+                        TotalNumberOfLineItemQty += QtyShipped;
+                    }
+                    Id++;
+                    Pid = 1;
                 }
-
-                WriteN1Loop1_arclient(EntityCode1.ST, EntityCode2.BuyerCode);
-                //WriteN1Loop1_ffaddr(Data, EntityCode1.ST);
-                //WriteN1Loop1_wscie(EntityCode1.VN);
-
 
                 //CTT Loop
                 WriteSegmentLoop("CTTLoop1", "Loop", "CTT", "Segment",
@@ -254,76 +286,76 @@ namespace EDI_RSS.Helpers
                     "HL04 : Hierarchical Child Code: Fixed : Additional Subordinate HL Data Segment in This Hierarchical Structure", "1");
 
                 WriteSegment("TD1", "Segment",
-                    "TD101 : Packaging Code: Fixed : Pallet: Standard", "PLT90",
-                    "TD102 : Lading Quantity", "1"); //<--
+                    "TD101 : Packaging Code: Fixed : Unit: Standard", "UNT90",
+                    "TD102 : Lading Quantity: cobili_qty", data["cobili_qty"].ToString());
 
                 WriteSegment("TD5", "Segment",
                     "TD501 : Routing Sequence Code: Fixed : Origin Carrier (Air, Motor, or Ocean)", "O",
-                    "TD502 : Identification Code Qualifier: Fixed : Standard Carrier Alpha Code (SCAC)", "2",
-                    "TD503 : Identification Code", "",  //<--
+                    "TD502 : Identification Code Qualifier: Fixed : Mutually Defined", "ZZ",
+                    "TD503 : Identification Code", "", 
                     "TD504 : Transportation Method/Type Code: Fixed : Supplier Truck", "SR");
 
                 //BM
                 WriteSegment("REF", "Segment",
                   "REF01 : Reference Identification Qualifier : Bill of Lading Number", "BM",
-                  "REF02 : Reference Identification : ", ""); //<--
+                  "REF02 : Reference Identification : cobil_ident", data["cobil_ident"].ToString());
 
                 //CR
                 WriteSegment("REF", "Segment",
                   "REF01 : Reference Identification Qualifier: Customer Reference Number", "CR",
-                  "REF02 : Reference Identification : ", ""); //<--
+                  "REF02 : Reference Identification : cobil_clientid", data["cobil_clientid"].ToString()); 
             }
             writer.WriteEndElement(); //HLLoop1
         } // method WriteHLLoop1_Shipment()
 
-        public void WriteHLLoop1_Order(IDataRecord data)
+        public void WriteHLLoop1_Order(IDataRecord data, int i, int j)
         {
             writer.WriteStartElement("HLLoop1");
             writer.WriteAttributeString("type", "Loop");
             {
                 WriteSegment("HL", "Segment",
-                    "HL01 : Hierarchical ID Number", "2", 
-                    "HL02 : Hierarchical Parent ID Number", "1", 
+                    "HL01 : Hierarchical ID Number", i.ToString(), 
+                    "HL02 : Hierarchical Parent ID Number", j.ToString(), 
                     "HL03 : Hierarchical Level Code: Fixed : Order", "O",
                     "HL04 : Hierarchical Child Code: Fixed : Additional Subordinate HL Data Segment in This Hierarchical Structure", "1");
 
                 WriteSegment("PRF", "Segment",
-                    "PRF01 : Purchase Order Number", "PO_1234_NUMBER",
+                    "PRF01 : Purchase Order Number: cocom_clientpo", data["cocom_clientpo"].ToString(),
                     "PRF02 : Release Number", "",
                     "PRF03 : Change Order Sequence Number", "",
-                    "PRF04 : Date", "20180910");
+                    "PRF04 : Date: cocom_co_dte", string.Format("{0:yyyyMMdd}", data["cocom_co_dte"])); 
 
                 WriteSegment("REF", "Segment",
                   "REF01 : Reference Identification Qualifier: Vendor Order Number", "VN",
-                  "REF02 : Reference Identification : ", "VN_1234_NUMBER"); //<--
+                  "REF02 : Reference Identification : cocom_ident", data["cocom_ident"].ToString());
             }
             writer.WriteEndElement(); //HLLoop1
         } //method WriteHLLoop1_Order()
 
-        public void WriteHLLoop1_Item(IDataRecord data)
+        public void WriteHLLoop1_Item(IDataRecord data, int qtyShippedDate, int i, int j)
         {
             writer.WriteStartElement("HLLoop1");
             writer.WriteAttributeString("type", "Loop");
             {
                 WriteSegment("HL", "Segment",
-                    "HL01 : Hierarchical ID Number", "3", 
-                    "HL02 : Hierarchical Parent ID Number", "2",
+                    "HL01 : Hierarchical ID Number", i.ToString(), 
+                    "HL02 : Hierarchical Parent ID Number", j.ToString(),
                     "HL03 : Hierarchical Level Code: Fixed : Item", "I",
                     "HL04 : Hierarchical Child Code: Fixed :  No Subordinate HL Segment in This Hierarchical Structure.", "0");
 
                 WriteSegment("LIN", "Segment",
-                    "LIN01 : Assigned Identification", "10",
+                    "LIN01 : Assigned Identification calc: cobili_line * 10", (Convert.ToInt32(data["cobili_line"].ToString()) * 10).ToString(),
                     "LIN02 : Product/Service ID Qualifier: Buyer's Part Number", "BP",
-                    "LIN03 : Product/Service ID", "BP_1234_NUMBER",
+                    "LIN03 : Product/Service ID : ivprixdcli_codecli", data["ivprixdcli_codecli"].ToString(),
                     "LIN04 : Product/Service ID Qualifier: Vendor's (Seller's) Part Number", "VP",
-                    "LIN05 : Product/Service ID", "VP_1234_NUMBER");
+                    "LIN05 : Product/Service ID : ivprod_code", data["ivprod_code"].ToString());
 
                 WriteSegment("SN1", "Segment",
-                    "SN101 : Assigned Identification", "10",
-                    "SN102 : Number of Units Shipped", "1100",
+                    "SN101 : Assigned Identification calc: cobili_line * 10", (Convert.ToInt32(data["cobili_line"].ToString()) * 10).ToString(),
+                    "SN102 : Number of Units Shipped: cobili_qty", data["cobili_qty"].ToString(),
                     "SN103 : Unit or Basis for Measurement Code: Each", "EA",
-                    "SN104 : Quantity Shipped to Date", "0",
-                    "SN105 : Quantity Ordered", "1100",
+                    "SN104 : Quantity Shipped to Date : calc: qtyShippedDate", qtyShippedDate.ToString(),
+                    "SN105 : Quantity Ordered: cobili_qty", data["cobili_qty"].ToString(),
                     "SN106 : Unit or Basis for Measurement Code: Each", "EA");
             }
             writer.WriteEndElement(); //HLLoop1
